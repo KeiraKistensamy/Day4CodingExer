16cba697e3344398f914504fbf67879f546200a6
KeiraKistensamy <Keirakist0987@gmail.com>
2024-05-17 09:25:15 +0200

    Question1

---
+++ script/code.js
@@ -0,0 +1,9 @@
+// Question1
+
+// let hours = 190
+// let bonus = hours > 180
+//     ? 'eligible for bonus'
+//     : 'not eligible for bonus'
+// console.log(`you have ${hours} and you are ${bonus}`);
+
+// *******************************************************************
